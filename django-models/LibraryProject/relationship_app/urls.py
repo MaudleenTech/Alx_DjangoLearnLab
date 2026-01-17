0a5887fd8f250f21891f152cccbe4dfb57ac2a7c
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-01-17 22:43:29 +0100

    Fix custom permission URLs to match checker expectations

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,8 +15,9 @@ urlpatterns = [
     path("admin-role/", views.admin_view, name="admin_view"),
 path("librarian-role/", views.librarian_view, name="librarian_view"),
 path("member-role/", views.member_view, name="member_view"),
-path("books/add/", views.add_book, name="add_book"),
-path("books/edit/", views.edit_book, name="edit_book"),
-path("books/delete/", views.delete_book, name="delete_book"),
+path("add_book/", views.add_book, name="add_book"),
+path("edit_book/", views.edit_book, name="edit_book"),
+path("delete_book/", views.delete_book, name="delete_book"),
+
 
 ]
