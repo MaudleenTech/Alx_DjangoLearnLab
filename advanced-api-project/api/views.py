dc269d3ae5da8f3ed4f5f7ad8e22ebcd7c8e008f
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-02-07 15:18:16 +0100

    Fix Task 1: apply IsAuthenticatedOrReadOnly and IsAuthenticated permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
