76933258139ab911b2ca107c552191f1a8a3a14a
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-02-19 22:16:46 +0100

    Add new features for testing

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    return render(request, "blog/posts.html")
+
+def login_view(request):
+    return render(request, "blog/login.html")
+
+def register(request):
+    return render(request, "blog/register.html")
