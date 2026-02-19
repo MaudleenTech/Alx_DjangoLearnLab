8d477cf5a9f2cb640fa6d5fb36f01d10a57b31fc
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-02-19 23:54:50 +0100

    Fix file path for ALX checker

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("posts/", views.posts, name="posts"),
 
     # auth pages (REQUIRED PATHS)
-    path("login", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
-    path("logout", auth_views.LogoutView.as_view(), name="logout"),
-    path("register", views.register, name="register"),
-    path("profile", views.profile, name="profile"),
+    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 ]
