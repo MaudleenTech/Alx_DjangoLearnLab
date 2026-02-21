f39d98d7bc96dc89ccde16774615acfcf9b3d71b
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-02-22 00:48:46 +0100

    URL Configuration for New Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,4 +27,5 @@ path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="commen
 
 path("tags/<slug:tag_slug>/", views.TagPostListView.as_view(), name="tag_posts"),
 path("search/", views.search, name="search"),
+path("tags/<str:tag>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -129,13 +129,14 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def get_success_url(self):
         return reverse("post_detail", kwargs={"pk": self.object.post.pk})
     
-class TagPostListView(ListView):
+class PostByTagListView(ListView):
     model = Post
     template_name = "blog/tag_posts.html"
     context_object_name = "posts"
 
     def get_queryset(self):
-        return Post.objects.filter(tags__slug=self.kwargs["tag_slug"])
+        tag = self.kwargs.get("tag")
+        return Post.objects.filter(tags__name__icontains=tag)
     
 def search(request):
     query = request.GET.get("q", "").strip()
