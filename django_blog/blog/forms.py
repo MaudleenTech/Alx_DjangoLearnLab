aaa9c0548f10cd1aabf294a64545b4da91a7effc
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-02-22 00:32:39 +0100

    Add search results feature

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -38,3 +38,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("title", "content", "tags")
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': TagWidget(),
+        }
